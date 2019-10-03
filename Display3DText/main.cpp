6de4f388436a94e5b5d2be6280f00c46c94867b7
charon616 <milkyway.k16@gmail.com>
2019-10-03 14:08:31 +0900

    Initial Commit

---
+++ Display3DText/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Display3DText
+//
+//  Created by 木方夏麟 on 2019/10/03.
+//  Copyright © 2019 木方夏麟. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
